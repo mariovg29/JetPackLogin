7c3b366066eb3319a3afc408fb01c6d6ff21341c
mariovg29 <ikarochacalguerrero@gmail.com>
2023-08-29 14:51:23 +0200

    Data part 1

---
+++ app/src/main/java/com/mariovg/jetpacklogin/login/data/network/LoginClient.kt
@@ -0,0 +1,10 @@
+package com.mariovg.jetpacklogin.login.data.network
+
+import com.mariovg.jetpacklogin.login.data.network.response.LoginResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface LoginClient {
+    @GET("/v3/f78c9d3-28b-481-9cf1-6d6ff78fa014")
+    suspend fun doLogin(user: String, password: String):Response<LoginResponse>
+}
